2a430ec0734b4f063ef9206ed86123652e7d9af3
Arya Yudhistira <arya.121140115@student.itera.ac.id>
2023-12-16 23:34:09 +0700

    fix db_connect for localhost

--- db_connect.php
+++ db_connect.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost = "localhost";
-$dbuser = "id21680358_aryayudhistira";
-$dbpass = "Smartfren123!";
-$db = "id21680358_barokah";
+$dbuser = "";
+$dbpass = "";
+$db = "barokah";
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
